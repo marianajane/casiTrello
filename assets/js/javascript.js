1ae2d8c3df3851f7c0ccf516ef6d37c007f46a93
marianajane <mariana.musm@gmail.com>
2017-03-29 10:48:10 -0600

    En desarrollo

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,9 +1,25 @@
-console.log("Go bitches!");
+console.log("We can do this!");
 
-function agregarL1(){
-	var node = document.createElement("LI");                 // Crea un nodo <li>
-	var textnode = document.createTextNode("Hacer un Trello bebé");         // Crea texto
-	node.appendChild(textnode);                              // Agrega texto a la lista <li>
-	document.getElementById("listaPendientes1").appendChild(node);  
+function agregarL1() {
+    var listaPendientes1= document.getElementById("lista1").value;
+    console.log (listaPendientes1);
+    var laLista = "<li>" + listaPendientes1 + "</li>";
+    document.getElementById("listaPendientes1").innerHTML += laLista;
+  }
 
-}
+function agregarL2() {
+    var listaPendientes2= document.getElementById("lista2").value;
+    console.log (listaPendientes2);
+    var laLista = "<li>" + listaPendientes2 + "</li>";
+    document.getElementById("listaPendientes2").innerHTML += laLista;
+  }
+
+  function crearLista() {
+  	var node = document.createElement("P");                 // Asignamos a una variable la creación de un nodo p
+	var textnode = document.getElementById("creandoNuevaLista").value;  
+	console.log(textnode); // Agrega texto a la lista <li>
+	var noIdea = node.appendChild(textnode); // Crea texto node.appendChild(textnode); 
+	console.log(noIdea);
+	//var nuevoNodo= document.getElementById("listaNueva");
+	//nuevoNodo.appendChild(node);
+  }

c92d2e59f180e8cca3eb7126e0feb2d2f1d6b138
marianajane <mariana.musm@gmail.com>
2017-03-29 12:22:02 -0600

    Desarrollar mas funciones

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -15,11 +15,29 @@ function agregarL2() {
   }
 
   function crearLista() {
-  	var node = document.createElement("P");                 // Asignamos a una variable la creación de un nodo p
-	var textnode = document.getElementById("creandoNuevaLista").value;  
-	console.log(textnode); // Agrega texto a la lista <li>
-	var noIdea = node.appendChild(textnode); // Crea texto node.appendChild(textnode); 
-	console.log(noIdea);
-	//var nuevoNodo= document.getElementById("listaNueva");
-	//nuevoNodo.appendChild(node);
+  	var listaNueva= document.getElementById("creandoNuevaLista").value;
+    console.log (listaNueva);
+    var laLista = "<h3>" + listaNueva + "</h3>";
+    document.getElementById("listaNueva").innerHTML += laLista;
+    
+    var aqui = document.createElement("LI");
+    var cuadrito = document.createElement("INPUT");
+    	cuadrito.setAttribute("type", "text");
+    	cuadrito.setAttribute("id", "otroPendiente");
+    	cuadrito.setAttribute("list", "li");
+    document.body.appendChild(cuadrito);
+    var boton = document.createElement("INPUT");
+    	boton.setAttribute("type", "button");
+    	boton.setAttribute("onclick", "agregarPendiente();");
+    	boton.setAttribute("name", "Crear pendiente");
+    	boton.setAttribute("value", "Crear pendiente");
+    document.body.appendChild(boton);
   }
+
+function agregarPendiente() {
+	var listaPendientesN= document.getElementById("otroPendiente").value;
+    console.log (listaPendientesN);
+    var laLista = "<li>" + listaPendientesN + "</li>";
+    document.getElementById("listaPendientesN").innerHTML += laLista;
+}
+    //Me hace falta crear nueva etiquea por cada vez que se crea nueva lista
